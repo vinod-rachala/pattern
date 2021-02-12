01b7347e22f17405eb0f37e85181e39f247ef6d7
vinod-rachala <37009777+vinod-rachala@users.noreply.github.com>
2021-02-11 22:35:54 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,40 @@
-# example of using the ColumnTransformer for the Abalone dataset
-from numpy import mean
-from numpy import std
-from numpy import absolute
-from pandas import read_csv
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.svm import SVR
- 
-# load dataset
-url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/abalone.csv'
-dataframe = read_csv(url, header=None)
-# split into inputs and outputs
-last_ix = len(dataframe.columns) - 1
-X, y = dataframe.drop(last_ix, axis=1), dataframe[last_ix]
-print(X.shape, y.shape)
-# determine categorical and numerical features
-numerical_ix = X.select_dtypes(include=['int64', 'float64']).columns
-categorical_ix = X.select_dtypes(include=['object', 'bool']).columns
-# define the data preparation for the columns
-t = [('cat', OneHotEncoder(), categorical_ix), ('num', MinMaxScaler(), numerical_ix)]
-col_transform = ColumnTransformer(transformers=t)
-# define the model
-model = SVR(kernel='rbf',gamma='scale',C=100)
-# define the data preparation and modeling pipeline
-pipeline = Pipeline(steps=[('prep',col_transform), ('m', model)])
-# define the model cross-validation configuration
-cv = KFold(n_splits=10, shuffle=True, random_state=1)
-# evaluate the pipeline using cross validation and calculate MAE
-scores = cross_val_score(pipeline, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
-# convert MAE scores to positive values
-scores = absolute(scores)
-# summarize the model performance
-print('MAE: %.3f (%.3f)' % (mean(scores), std(scores)))
+# K-Means Clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+# Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Training the K-Means model on the dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
